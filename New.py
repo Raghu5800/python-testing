1a50c001b4950f5a823d2805d48223ce6f6c756d
Raghuraman <raghu@MacBookAir.lan>
2025-10-08 00:40:41 +0530

    hello1

---
+++ New.py
@@ -0,0 +1,10 @@
+a = int(input("value of a is:"))
+b = int(input("value b is:"))
+
+if a%2 ==0:
+    print(a,"is even")
+elif a%2 ==1:
+    print(b,"is odd")
+
+c = a+b
+print("Sum is:",c)
---
+++ New2.py
@@ -0,0 +1,9 @@
+a = str(input("username"))
+b = str(input("password"))
+
+if a == "raghu":
+    #print ("username exist now check password")
+    if b == "admin":
+        print("user logged in")
+else:
+    print("user does not exist")
